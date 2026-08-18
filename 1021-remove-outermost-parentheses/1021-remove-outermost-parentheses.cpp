7e68856c7857d44b09cfda1d24c7c0db7fbf1e27
Gadi Srihari Reddy <sriharireddygadi@gmail.com>
2026-08-18 21:38:37 +0530

    Time: 3 ms (17.13%), Space: 9 MB (23.53%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        string ans="";
+        for(int i = 0; i < s.size(); i++) { 
+            if(s[i] == '(') { 
+                if(count > 0) { 
+                    ans.push_back(s[i]); 
+                } 
+                count++; 
+            } else { 
+                count--; 
+            
+                if(count > 0) { 
+                    ans.push_back(s[i]); 
+                } 
+            } 
+        } 
+        return ans;
+    }
+};
