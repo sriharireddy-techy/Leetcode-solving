7af1cff8d3d03800001c45ae4e384620cd1b9397
Gadi Srihari Reddy <sriharireddygadi@gmail.com>
2026-09-03 17:59:39 +0530

    Time: 4 ms (77.55%), Space: 128.3 MB (54.21%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       
-        vector<int> res;
-        vector<int> pos;
-        vector<int> neg;
-        for(int i:nums){
-            if(i>=0){
-                pos.push_back(i);
-            }else{
-                neg.push_back(i);
-            }
-        }
-        int p=0;
-        int n=0;
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0){
-                res.push_back(pos[p]);
-                p++;
-            }else{
-                res.push_back(neg[n]);
-                n++;
 
+        vector<int> res(nums.size());
+        int pos=0;
+        int neg=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0){
+                res[pos]=nums[i];
+                pos=pos+2;
+            }else{
+                res[neg]=nums[i];
+                neg=neg+2;
             }
         }
-        return res;
-        
+        return res;    
     }
 };
